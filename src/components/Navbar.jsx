174d0aa8cb39740feb14bc0789f6df25b89e1e28
Umar-Waseem <umar.waseem@gmail.com>
2024-10-09 19:14:07 +0500

    update: navbar animations and font

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,32 @@
 import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom'; // Add this import
 
 export default function Navbar({ links }) {
     const [menuOpen, setMenuOpen] = useState(false);
+    const location = useLocation(); // Add this line
 
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
     };
 
     return (
-        <nav className="border-gray-200 bg-customGreen">
+        <nav className="border-gray-200 bg-customGreen font-serif">
             <div className="md:px-24 flex md:flex-row flex-col items-center mx-auto p-2 bg-customGreen">
                 <div className={`w-full md:block ${menuOpen ? 'block' : 'hidden'}`} id="navbar-default">
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border bg-customGreen border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
                         {links.map((link, index) => (
                             <li key={index}>
-                                <a href={link.href} className="block py-2 px-3 text-white hover:text-customYellow">{link.label}</a>
+                                <a 
+                                    href={link.href} 
+                                    className={`block py-2 px-3 text-white text-xl hover:text-customYellow relative
+                                        ${location.pathname === link.href ? 'text-customYellow font-bold' : ''}
+                                        after:content-[''] after:absolute after:w-full after:h-0.5 after:bg-customYellow after:bottom-0 after:left-0
+                                        after:scale-x-0 after:origin-bottom-right after:transition-transform after:duration-300 after:ease-out
+                                        ${location.pathname === link.href ? 'after:scale-x-100 after:origin-bottom-left' : ''}
+                                        hover:after:scale-x-100 hover:after:origin-bottom-left`}
+                                >
+                                    {link.label}
+                                </a>
                             </li>
                         ))}
                     </ul>
